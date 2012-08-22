6273eea95d39ab9e03c15ddf6fff7f720c3cd8cb
walyong.cho <walyong.cho@samsung.com>
2012-08-22 15:00:47 +0900

    tizen 2.0

--- poptparse.c
+++ poptparse.c
@@ -10,7 +10,6 @@
 
 #define POPT_ARGV_ARRAY_GROW_DELTA 5
 
-/*@-boundswrite@*/
 int poptDupArgv(int argc, const char **argv,
 		int * argcPtr, const char *** argvPtr)
 {
@@ -32,13 +31,13 @@ int poptDupArgv(int argc, const char **argv,
 	return POPT_ERROR_MALLOC;
     argv2 = (void *) dst;
     dst += (argc + 1) * sizeof(*argv);
+    *dst = '\0';
 
-    /*@-branchstate@*/
     for (i = 0; i < argc; i++) {
 	argv2[i] = dst;
-	dst += strlen(strcpy(dst, argv[i])) + 1;
+	dst = stpcpy(dst, argv[i]);
+	dst++;	/* trailing NUL */
     }
-    /*@=branchstate@*/
     argv2[argc] = NULL;
 
     if (argvPtr) {
@@ -51,9 +50,7 @@ int poptDupArgv(int argc, const char **argv,
 	*argcPtr = argc;
     return 0;
 }
-/*@=boundswrite@*/
 
-/*@-bounds@*/
 int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
 {
     const char * src;
@@ -61,11 +58,16 @@ int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
     int argvAlloced = POPT_ARGV_ARRAY_GROW_DELTA;
     const char ** argv = malloc(sizeof(*argv) * argvAlloced);
     int argc = 0;
-    int buflen = strlen(s) + 1;
-    char * buf = memset(alloca(buflen), 0, buflen);
+    size_t buflen = strlen(s) + 1;
+    char * buf, * bufOrig = NULL;
     int rc = POPT_ERROR_MALLOC;
 
     if (argv == NULL) return rc;
+    buf = bufOrig = calloc((size_t)1, buflen);
+    if (buf == NULL) {
+	free(argv);
+	return rc;
+    }
     argv[argc] = buf;
 
     for (src = s; *src != '\0'; src++) {
@@ -81,7 +83,7 @@ int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
 		if (*src != quote) *buf++ = '\\';
 	    }
 	    *buf++ = *src;
-	} else if (isspace(*src)) {
+	} else if (_isspaceptr(src)) {
 	    if (*argv[argc] != '\0') {
 		buf++, argc++;
 		if (argc == argvAlloced) {
@@ -116,29 +118,29 @@ int poptParseArgvString(const char * s, int * argcPtr, const char *** argvPtr)
     rc = poptDupArgv(argc, argv, argcPtr, argvPtr);
 
 exit:
+    if (bufOrig) free(bufOrig);
     if (argv) free(argv);
     return rc;
 }
-/*@=bounds@*/
 
 /* still in the dev stage.
  * return values, perhaps 1== file erro
  * 2== line to long
  * 3== umm.... more?
  */
-int poptConfigFileToString(FILE *fp, char ** argstrp, /*@unused@*/ int flags)
+int poptConfigFileToString(FILE *fp, char ** argstrp,
+		/*@unused@*/ UNUSED(int flags))
 {
     char line[999];
     char * argstr;
     char * p;
     char * q;
     char * x;
-    int t;
-    int argvlen = 0;
+    size_t t;
+    size_t argvlen = 0;
     size_t maxlinelen = sizeof(line);
     size_t linelen;
-    int maxargvlen = 480;
-    int linenum = 0;
+    size_t maxargvlen = (size_t)480;
 
     *argstrp = NULL;
 
@@ -153,34 +155,35 @@ int poptConfigFileToString(FILE *fp, char ** argstrp, /*@unused@*/ int flags)
     if (argstr == NULL) return POPT_ERROR_MALLOC;
 
     while (fgets(line, (int)maxlinelen, fp) != NULL) {
-	linenum++;
 	p = line;
 
 	/* loop until first non-space char or EOL */
-	while( *p != '\0' && isspace(*p) )
+	while( *p != '\0' && _isspaceptr(p) )
 	    p++;
 
 	linelen = strlen(p);
-	if (linelen >= maxlinelen-1)
+	if (linelen >= maxlinelen-1) {
+	    free(argstr);
 	    return POPT_ERROR_OVERFLOW;	/* XXX line too long */
+	}
 
 	if (*p == '\0' || *p == '\n') continue;	/* line is empty */
 	if (*p == '#') continue;		/* comment line */
 
 	q = p;
 
-	while (*q != '\0' && (!isspace(*q)) && *q != '=')
+	while (*q != '\0' && (!_isspaceptr(q)) && *q != '=')
 	    q++;
 
-	if (isspace(*q)) {
+	if (_isspaceptr(q)) {
 	    /* a space after the name, find next non space */
 	    *q++='\0';
-	    while( *q != '\0' && isspace((int)*q) ) q++;
+	    while( *q != '\0' && _isspaceptr(q) ) q++;
 	}
 	if (*q == '\0') {
 	    /* single command line option (ie, no name=val, just name) */
 	    q[-1] = '\0';		/* kill off newline from fgets() call */
-	    argvlen += (t = q - p) + (sizeof(" --")-1);
+	    argvlen += (t = (size_t)(q - p)) + (sizeof(" --")-1);
 	    if (argvlen >= maxargvlen) {
 		maxargvlen = (t > maxargvlen) ? t*2 : maxargvlen*2;
 		argstr = realloc(argstr, maxargvlen);
@@ -197,18 +200,18 @@ int poptConfigFileToString(FILE *fp, char ** argstrp, /*@unused@*/ int flags)
 	*q++ = '\0';
 
 	/* find next non-space letter of value */
-	while (*q != '\0' && isspace(*q))
+	while (*q != '\0' && _isspaceptr(q))
 	    q++;
 	if (*q == '\0')
 	    continue;	/* XXX silently ignore missing value */
 
 	/* now, loop and strip all ending whitespace */
 	x = p + linelen;
-	while (isspace(*--x))
-	    *x = 0;	/* null out last char if space (including fgets() NL) */
+	while (_isspaceptr(--x))
+	    *x = '\0';	/* null out last char if space (including fgets() NL) */
 
 	/* rest of line accept */
-	t = x - p;
+	t = (size_t)(x - p);
 	argvlen += t + (sizeof("' --='")-1);
 	if (argvlen >= maxargvlen) {
 	    maxargvlen = (t > maxargvlen) ? t*2 : maxargvlen*2;
--- testit.sh
+++ testit.sh
@@ -7,9 +7,9 @@ run() {
 
     echo Running test $name.
 
-    result=`$builddir/$prog $*`
+    result=`HOME=$builddir $builddir/$prog $*`
     if [ "$answer" != "$result" ]; then
-	echo "Test \"$*\" failed with: \"$result\" != \"$answer\" "
+	echo "Test \"$prog $*\" failed with: \"$result\" != \"$answer\" "
 	exit 2
     fi
 }
@@ -76,25 +76,104 @@ run test1 "test1 - 23" "--echo-args -a" --echo-args -e -a
 run test1 "test1 - 24" "arg1: 0 arg2: (none) short: 1" -onedash
 run test1 "test1 - 25" "arg1: 0 arg2: (none) short: 1" --onedash
 run test1 "test1 - 26" "callback: c arg for cb2 foo arg1: 0 arg2: (none)" --cb2 foo
-run test1 "test1 - 27" "arg1: 0 arg2: (none) -" -
-run test1 "test1 - 28" "arg1: 0 arg2: foo -" - -2 foo
+run test1 "test1 - 27" "arg1: 0 arg2: (none) rest: -" -
+run test1 "test1 - 28" "arg1: 0 arg2: foo rest: -" - -2 foo
 run test1 "test1 - 29" "arg1: 0 arg2: bbbb" --arg2=aaaa -2 bbbb
 run test1 "test1 - 30" "arg1: 0 arg2: 'foo bingo' rest: boggle" --grab bingo boggle
 run test1 "test1 - 31" "arg1: 0 arg2: 'foo bar' rest: boggle" --grabbar boggle
 
-run test1 "test1 - 32" "arg1: 0 arg2: (none) aFloat: 10.1" -f 10.1
-run test1 "test1 - 33" "arg1: 0 arg2: (none) aFloat: 10.1" --float 10.1
-run test1 "test1 - 34" "arg1: 0 arg2: (none) aDouble: 10.1" -d 10.1
-run test1 "test1 - 35" "arg1: 0 arg2: (none) aDouble: 10.1" --double 10.1
-run test1 "test1 - 36" "arg1: 0 arg2: (none) oStr: (none)" --optional
-run test1 "test1 - 37" "arg1: 0 arg2: (none) oStr: yadda" --optional=yadda
-run test1 "test1 - 38" "arg1: 0 arg2: (none) oStr: yadda" --optional yadda
-run test1 "test1 - 39" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional=ping pong
-run test1 "test1 - 40" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional ping pong
+run test1 "test1 - 32" "arg1: 0 arg2: (none) aInt: 123456789" -i 123456789
+run test1 "test1 - 33" "arg1: 0 arg2: (none) aInt: 123456789" --int 123456789
+run test1 "test1 - 34" "arg1: 0 arg2: (none) aShort: 12345" -s 12345
+run test1 "test1 - 35" "arg1: 0 arg2: (none) aShort: 12345" --short 12345
+run test1 "test1 - 36" "arg1: 0 arg2: (none) aLong: 1123456789" -l 1123456789
+run test1 "test1 - 37" "arg1: 0 arg2: (none) aLong: 1123456789" --long 1123456789
+run test1 "test1 - 38" "arg1: 0 arg2: (none) aLongLong: 1111123456789" -L 1111123456789
+run test1 "test1 - 39" "arg1: 0 arg2: (none) aLongLong: 1111123456789" --longlong 1111123456789
 
-run_diff test3 "test3 - 41" test3-data/01.input test3-data/01.answer
-run_diff test3 "test3 - 42" test3-data/02.input test3-data/02.answer
-run_diff test3 "test3 - 43" test3-data/03.input test3-data/03.answer
+run test1 "test1 - 40" "arg1: 0 arg2: (none) aFloat: 10.1" -f 10.1
+run test1 "test1 - 41" "arg1: 0 arg2: (none) aFloat: 10.1" --float 10.1
+run test1 "test1 - 42" "arg1: 0 arg2: (none) aDouble: 10.1" -d 10.1
+run test1 "test1 - 43" "arg1: 0 arg2: (none) aDouble: 10.1" --double 10.1
+
+run test1 "test1 - 44" "arg1: 0 arg2: (none) oStr: (none)" --optional
+run test1 "test1 - 45" "arg1: 0 arg2: (none) oStr: yadda" --optional=yadda
+run test1 "test1 - 46" "arg1: 0 arg2: (none) oStr: yadda" --optional yadda
+run test1 "test1 - 47" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional=ping pong
+run test1 "test1 - 48" "arg1: 0 arg2: (none) oStr: ping rest: pong" --optional ping pong
+run test1 "test1 - 49" "arg1: 0 arg2: (none) aArgv: A B rest: C" --argv A --argv B C
+
+run test1 "test1 - 50" "arg1: 0 arg2: foo=bar" -2foo=bar
+run test1 "test1 - 51" "arg1: 0 arg2: foo=bar" -2=foo=bar
+
+run test1 "test1 - 52" "arg1: 0 arg2: (none) aFlag: 0xfeed" --bitxor
+run test1 "test1 - 53" "arg1: 0 arg2: (none) aFlag: 0xffff" --bitset
+run test1 "test1 - 54" "arg1: 0 arg2: (none) aFlag: 0x28c" --bitclr
+run test1 "test1 - 55" "arg1: 0 arg2: (none) aFlag: 0x8888" --nobitset
+run test1 "test1 - 56" "arg1: 0 arg2: (none) aFlag: 0xface" --nobitclr
+
+run test1 "test1 - 57" "arg1: 0 arg2: (none) aBits: foo,baz" --bits foo,bar,baz,!bar
+
+run test1 "test1 - 58" "\
+Usage: lt-test1 [-I?] [-c|--cb2=STRING] [--arg1] [-2|--arg2=ARG]
+        [-3|--arg3=ANARG] [-onedash] [--optional=STRING] [--val]
+        [-i|--int=INT] [-s|--short=SHORT] [-l|--long=LONG]
+        [-L|--longlong=LONGLONG] [-f|--float=FLOAT] [-d|--double=DOUBLE]
+        [--randint=INT] [--randshort=SHORT] [--randlong=LONG]
+        [--randlonglong=LONGLONG] [--argv=STRING] [--bitset] [--bitclr]
+        [--bitxor] [--nstr=STRING] [--lstr=STRING] [-I|--inc]
+        [-c|--cb=STRING] [--longopt] [-?|--help] [--usage] [--simple=ARG]" --usage
+run test1 "test1 - 59" "\
+Usage: lt-test1 [OPTION...]
+      --arg1                      First argument with a really long
+                                  description. After all, we have to test
+                                  argument help wrapping somehow, right?
+  -2, --arg2=ARG                  Another argument (default: \"(none)\")
+  -3, --arg3=ANARG                A third argument
+      -onedash                    POPT_ARGFLAG_ONEDASH: Option takes a single -
+      --optional[=STRING]         POPT_ARGFLAG_OPTIONAL: Takes an optional
+                                  string argument
+      --val                       POPT_ARG_VAL: 125992 141421
+  -i, --int=INT                   POPT_ARG_INT: 271828 (default: 271828)
+  -s, --short=SHORT               POPT_ARG_SHORT: 4523 (default: 4523)
+  -l, --long=LONG                 POPT_ARG_LONG: 738905609 (default: 738905609)
+  -L, --longlong=LONGLONG         POPT_ARG_LONGLONG: 738905609 (default:
+                                  738905609)
+  -f, --float=FLOAT               POPT_ARG_FLOAT: 3.14159 (default: 3.14159)
+  -d, --double=DOUBLE             POPT_ARG_DOUBLE: 9.8696 (default: 9.8696)
+      --randint=INT               POPT_ARGFLAG_RANDOM: experimental
+      --randshort=SHORT           POPT_ARGFLAG_RANDOM: experimental
+      --randlong=LONG             POPT_ARGFLAG_RANDOM: experimental
+      --randlonglong=LONGLONG     POPT_ARGFLAG_RANDOM: experimental
+      --argv STRING               POPT_ARG_ARGV: append string to argv array
+                                  (can be used multiple times)
+      --[no]bitset                POPT_BIT_SET: |= 0x7777
+      --[no]bitclr                POPT_BIT_CLR: &= ~0xf842
+      --bitxor                    POPT_ARGFLAG_XOR: ^= (0x8ace^0xfeed)
+      --nstr=STRING               POPT_ARG_STRING: (null) (default: null)
+      --lstr=STRING               POPT_ARG_STRING: \"123456789...\" (default:
+                                  \"This tests default strings and exceeds the
+                                  ... limit.
+                                  123456789+123456789+123456789+123456789+123456789+ 123456789+123456789+123456789+123456789+123456789+ 1234567...\")
+
+arg for cb2
+  -c, --cb2=STRING                Test argument callbacks
+  -I, --inc                       An included argument
+
+Callback arguments
+  -c, --cb=STRING                 Test argument callbacks
+      --longopt                   Unused option for help testing
+
+Options implemented via popt alias/exec:
+      --simple=ARG                simple description
+
+Help options:
+  -?, --help                      Show this help message
+      --usage                     Display brief usage message" --help
+
+#run_diff test3 "test3 - 51" test3-data/01.input test3-data/01.answer
+#run_diff test3 "test3 - 52" test3-data/02.input test3-data/02.answer
+#run_diff test3 "test3 - 53" test3-data/03.input test3-data/03.answer
 
 echo ""
 echo "Passed."
